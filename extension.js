66af3d6ebdd4ef167b2d14cce35730ea96ff55d2
Bill <billjohnston4@gmail.com>
2019-12-16 22:00:01 +0000

    Multiple selections and title menu

--- extension.js
+++ extension.js
@@ -2,31 +2,12 @@
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode')
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-
-/**
- * @param {vscode.ExtensionContext} context
- */
-function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	// console.log('Congratulations, your extension "copy-import" is now active!')
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand(
-		'extension.copyImport',
-		function (options) {
-			// The code you place here will be executed every time your command is executed
-			// const root = vscode.workspace.workspaceFolders.uri.path
+const createImportString = (optionsArr) => {
+	return optionsArr.reduce((acc, options) => {
 		const filePath = options.path
 		const extensionSplit = filePath.split('.')
 		const extension = extensionSplit[extensionSplit.length - 1]
 		const config = vscode.workspace.getConfiguration('copyImport')
-
 		const template = config.fileExtensionImportTemplateMap[extension]
 		if (template) {
 			const splitPath = filePath.split('/')
@@ -46,15 +27,53 @@ function activate(context) {
 			const importString = template.replace(/\{\{\w+\}\}/g, function(all) {
 				return replacements[all] || all
 			})
-			vscode.env.clipboard.writeText(importString)
+			return `${acc}${acc ? '\n' : ''}${importString}`
+		} else {
 			vscode.window.showInformationMessage(
 				'Import copied to clipboard'
 			)
-		} else {
-			vscode.window.showErrorMessage(
-				`.${extension} file extension import template not configured`
-			)
-		}	
+			return acc
+		}
+	}, '')
+}
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	// console.log('Congratulations, your extension "copy-import" is now active!')
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with  registerCommand
+	// The commandId parameter must match the command field in package.json
+
+	// *** Can't figure out how to get a fuzzy finder ***
+	// const copyImportFromSearch = vscode.commands.registerCommand(
+	// 	'extension.copyImportSearch',
+	// 	function(options) {
+	// 		vscode.workspace.findFiles('*').then(console.log)
+	// 	}
+	// )
+
+	let disposable = vscode.commands.registerCommand(
+		'extension.copyImport',
+		function (options, allOptions) {
+		let allFiles = allOptions
+		if (!Array.isArray(allOptions)) {
+			allFiles = [options]
+		}
+		// The code you place here will be executed every time your command is executed
+		// const root = vscode.workspace.workspaceFolders.uri.path
+		const importString = createImportString(allFiles)
+		vscode.env.clipboard.writeText(importString)
+		vscode.window.showInformationMessage(
+			'Import copied to clipboard'
+		)
 	})
 
 	context.subscriptions.push(disposable)
